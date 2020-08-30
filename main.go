c6361d0b27faa7af874e7758a43fccdb19d74565
xuzequn <xzqyymt@126.com>
2020-08-30 23:47:24 +0800

    day5

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	_ "beegolearning/routers"
+	"fmt"
+
 	"github.com/astaxie/beego"
 )
 
 func main() {
+
+	httpport := beego.AppConfig.String("httpport")
+	fmt.Printf(httpport)
 	beego.Run()
 }
-
--- routers/router.go
+++ routers/router.go
@@ -11,6 +11,7 @@ func init() {
 	beego.Router("/user", &controllers.UserController{})
 	beego.Router("/test_static", &controllers.StaticController{})
 	// beego.Router("/params/?:id:int", &controllers.ParamsController{})
+	beego.Router("/params/?:id", &controllers.ParamsController{}, "get:GET")
 	beego.Router("/params", &controllers.ParamsController{})
 	beego.Router("/other_type_data", &controllers.OtherTypeDataContorller{})
 	beego.Router("/flash_data", &controllers.FlashController{})
